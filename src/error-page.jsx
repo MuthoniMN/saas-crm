ef26beb6adbe0e31f1758f651c7505302b0fd773
Michelle Ndiangui <ndianguimichelle@gmail.com>
2024-01-12 17:13:43 +0300

    not found default page

---
+++ src/error-page.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { useRouteError } from "react-router-dom";
+
+const Error = () => {
+    const err = useRouteError()
+    return (
+        <div style={{ textAlign: "center", width: "100vw" }}>
+            <h1>Ooops!</h1>
+            <p>We couldn't find what you're looking for.</p>
+            <p>
+                {err.statusText || err.message}
+            </p>
+        </div>
+    )
+}
+
+export default Error;
