e43a690460d1efc0317f93f6301bfb1bf90125a0
Michelle Ndiangui <ndianguimichelle@gmail.com>
2024-01-13 23:46:06 +0300

    add new dropdown

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSearch, faUser } from "@fortawesome/free-solid-svg-icons";
+import { faArrowRight, faSearch, faUser } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
+    const [dropdown, setDropdown] = useState(false)
     return (
         <div className="topNav">
             <h3>Dashboard</h3>
             <div>
-                <button>Add New <span style={{ marginLeft: "12px", fontSize: "20px" }}>+</span></button>
+                <button onClick={() => setDropdown(!dropdown)}>Add New <span style={{ marginLeft: "12px", fontSize: "20px" }}>+</span></button>
+                {dropdown && <div className="drop-down">
+                    <ul>
+                        <li><a href="/client/new">Client <FontAwesomeIcon icon={faArrowRight} /></a></li>
+                        <li><a href="/deal/new">Deal <FontAwesomeIcon icon={faArrowRight} /></a></li>
+                        <li><a href="/meeting/new">Meeting <FontAwesomeIcon icon={faArrowRight} /></a></li>
+                    </ul>
+                </div>}
                 <div>
                     <FontAwesomeIcon icon={faSearch} />
                 </div>
