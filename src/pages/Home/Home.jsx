99177e64762ad6e74d95478f8ca577f8decc5b6b
Michelle Ndiangui <ndianguimichelle@gmail.com>
2024-01-13 20:19:36 +0300

    removing components

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,10 @@
 import React from "react";
-import Sidebar from "../../components/Sidebar";
 
 const Home = () => {
     return (
-        <main>
-            <Sidebar />
-        </main>
+        <section>
+            Home
+        </section>
     )
 }
 
