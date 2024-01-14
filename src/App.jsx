9baf1fa57da95be34bdb26da4b743f96c7460c82
Michelle Ndiangui <ndianguimichelle@gmail.com>
2024-01-14 15:23:54 +0300

    /project/new page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Home from './pages/Home/Home.jsx'
 import Error from './error-page.jsx'
-import Deals from './pages/Deals/Deals.jsx'
 import './App.css'
 import Layout from './components/Layout'
 import AddClient from './pages/AddClient/AddClient.jsx'
+import AddProject from './pages/AddProject/AddProject.jsx'
 
 const router = createBrowserRouter([
   {
@@ -14,16 +14,16 @@ const router = createBrowserRouter([
         path: '/',
         element: <Home />,
         errorElement: <Error />
-      },
-      {
-        path: '/deals',
-        element: <Deals />
       }
     ]
   },
   {
     path: '/client/new',
     element: <AddClient />
+  },
+  {
+    path: '/project/new',
+    element: <AddProject />
   }
 
 ])
---
+++ src/pages/AddProject/AddProject.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import "./AddProject.css"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faXmarkCircle } from "@fortawesome/free-solid-svg-icons";
+import { useNavigate } from "react-router-dom";
+
+const AddProject = () => {
+    const [clients, setClients] = useState("")
+    const services = ["System Account", "Subscription"]
+    const navigate = useNavigate()
+    return (
+        <section className="container">
+            <section>
+                <header>
+                    <h2>Add a Project</h2>
+                    <FontAwesomeIcon icon={faXmarkCircle} onClick={() => navigate("/")} />
+                </header>
+                <form action="">
+                    <div className="inputContainer">
+                        <label htmlFor="client">Client: </label>
+                        <select id="client" className="select" >
+                            <option disabled selected>Choose a Client</option>
+                            {!clients && <option>No Clients Found</option>}
+                        </select>
+                    </div>
+                    <div className="inputContainer">
+                        <label htmlFor="service">Service: </label>
+                        <select id="service" className="select" >
+                            <option disabled selected>Choose a Service</option>
+                            {!services && <option>No Services Found</option>}
+                            {services.map(service => <option>{service}</option>)}
+                        </select>
+                    </div>
+                    <div className="inputContainer">
+                        <label htmlFor="desc">Service Description:</label>
+                        <textarea name="desc" id="desc" rows={3} cols={10} placeholder="This service includes the following deliverables: "></textarea>
+                    </div>
+                    <div className="inputContainer">
+                        <label htmlFor="dueDate">Due Date:</label>
+                        <input type="date" name="dueDate" id="dueDate" />
+                    </div>
+                    <div className="btnContainer">
+                        <button className="btnVariant" onClick={() => navigate("/")}>Cancel</button>
+                        <button type="submit">Add New Client</button>
+                    </div>
+                </form>
+            </section>
+        </section>
+    )
+}
+
+export default AddProject
